19fdcf95c3c6b8ef5ac4d3a7d0273e51546a323a
LUCY <LUCY@192.168.1.102>
2018-01-30 21:38:35 +0100

    Añadido Mensaje de bienvenida

--- src/servlets/Index.java
+++ src/servlets/Index.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -37,6 +39,8 @@ public class Index extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		out.println("<p> Bienvenido</p>");
 	}
 
 	/**
